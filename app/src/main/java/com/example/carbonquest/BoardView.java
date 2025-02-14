61dc2604db27f5416b61c7d84976dff54a7b3aaf
Antoine E. <78554532+antiX19@users.noreply.github.com>
2025-02-14 17:01:27 +0100

    Update BoardView.java

--- app/src/main/java/com/example/carbonquest/BoardView.java
+++ app/src/main/java/com/example/carbonquest/BoardView.java
@@ -39,7 +39,7 @@ public class BoardView extends View {
         tokenPlayer2 = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.luffy);
         tokenPlayer1 = Bitmap.createScaledBitmap(tokenPlayer1, tokenPlayer1.getWidth() / 2, tokenPlayer1.getHeight() / 2, true);
         tokenPlayer2 = Bitmap.createScaledBitmap(tokenPlayer2, tokenPlayer2.getWidth() / 2, tokenPlayer2.getHeight() / 2, true);
-        playerPositions = new int[]{20, 20};
+        playerPositions = new int[]{50, 700};
         setPlayerPositions(playerPositions );
 
     }
@@ -93,42 +93,43 @@ public class BoardView extends View {
 
     private int[] getCenterCoordinates(int position) {
         switch (position) {
-            default : return new int[]{700, 57};
-            // Top side
-            case 2: return new int[]{165, 57};
-            case 3: return new int[]{236, 57};
-            case 4: return new int[]{310, 57};
-            case 5: return new int[]{386, 57};
-            case 6: return new int[]{458, 57};
-            case 7: return new int[]{530, 57};
-            case 8: return new int[]{605, 57};
-            case 9: return new int[]{708, 57};
-            case 10: return new int[]{708, 156};
-            // Right side
-            case 11: return new int[]{708, 228};
-            case 12: return new int[]{708, 306};
-            case 13: return new int[]{708, 375};
-            case 14: return new int[]{708, 450};
-            case 15: return new int[]{708, 523};
-            case 16: return new int[]{708, 595};
-            case 17: return new int[]{708, 700};
+            default : return new int[]{50, 700};
             // Bottom side
-            case 18: return new int[]{605, 700};
-            case 19: return new int[]{530, 700};
-            case 20: return new int[]{458, 700};
-            case 21: return new int[]{386, 700};
-            case 22: return new int[]{310, 700};
-            case 23: return new int[]{236, 700};
-            case 24: return new int[]{165, 700};
+            case 1: return new int[]{50, 700};
+            case 2: return new int[]{165, 700};
+            case 3: return new int[]{236, 700};
+            case 4: return new int[]{310, 700};
+            case 5: return new int[]{386, 700};
+            case 6: return new int[]{458, 700};
+            case 7: return new int[]{530, 700};
+            case 8: return new int[]{605, 700};
+            // Right side
+            case 9: return new int[]{700, 700};
+            case 10: return new int[]{700, 600};
+            case 11: return new int[]{708, 523};
+            case 12: return new int[]{708, 450};
+            case 13: return new int[]{708, 375};
+            case 14: return new int[]{708, 310};
+            case 15: return new int[]{708, 233};
+            case 16: return new int[]{708, 151};
+            // Top side
+            case 17: return new int[]{722, 50};
+            case 18: return new int[]{605, 50};
+            case 19: return new int[]{530, 50};
+            case 20: return new int[]{458, 50};
+            case 21: return new int[]{386, 58};
+            case 22: return new int[]{310, 53};
+            case 23: return new int[]{236, 53};
+            case 24: return new int[]{165, 53};
             // Left side
-            case 25: return new int[]{65, 700};
-            case 26: return new int[]{65, 595};
-            case 27: return new int[]{65, 523};
-            case 28: return new int[]{65, 450};
-            case 29: return new int[]{65, 375};
-            case 30: return new int[]{65, 306};
-            case 31: return new int[]{65, 228};
-            case 32: return new int[]{65, 157};
+            case 25: return new int[]{56, 65};
+            case 26: return new int[]{56, 158};
+            case 27: return new int[]{56, 229};
+            case 28: return new int[]{56, 309};
+            case 29: return new int[]{56, 375};
+            case 30: return new int[]{56, 450};
+            case 31: return new int[]{56, 528};
+            case 32: return new int[]{56, 600};
         }
     }
 
